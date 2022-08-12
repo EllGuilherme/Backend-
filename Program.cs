1513914e4f3f27ae9408bc3740c7300336cdd233
Ellguilherme <grds1000@gmail.com>
2022-08-11 21:27:31 -0300

    Adicina Backend com API de rolagem de dados

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/dado/d{numeroDeFaces}",(
+  [FromRoute] int numeroDeFaces
+ )=>{
+    if (numeroDeFaces <= 0)
+    {
+        return Results.BadRequest(new { mensagem = "Somente dadaos com no minimo uma face"});
+    }
+    
+int face = RandomNumberGenerator.GetInt32(1, numeroDeFaces + 1);
+
+return Results.Ok(new { dado = $"d{numeroDeFaces}", rolagem = face});
+
+});
+
+app.Run();
+ 
+
